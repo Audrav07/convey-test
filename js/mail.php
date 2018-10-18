6bf2710a47994ba9e714625727983074d7fa419f
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-10-18 08:21:48 -0600

    changes

--- js/mail.php
+++ js/mail.php
@@ -22,30 +22,3 @@ echo "Mail Sent. Thanks you";
 
 
 
-<!-- // define variables and set to empty values
-$nameErr = $emailErr = "";
-$name = $email = $comment = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
-
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  
-}
-?> -->
